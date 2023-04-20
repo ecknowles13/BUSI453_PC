363935d88fcca8fa35de9b3f95252f56b7f5a665
ecriggins <ecknowles13@gmail.com>
2023-04-20 15:18:51 -0400

    Updates after meeting with Emory

--- Topic_Modeling_Supervised.py
+++ Topic_Modeling_Supervised.py
@@ -7,6 +7,14 @@ if __name__ == '__main__':
     import gensim
     from gensim.utils import simple_preprocess
     import nltk
+    import ssl
+
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
 
     nltk.download('stopwords')
     from nltk.corpus import stopwords
--- geocode_dist.py
+++ geocode_dist.py
@@ -9,8 +9,14 @@ for zipcode in df['ZIP']:
     tempdist = gdata.query_postal_code(24515, zipcode)
     distance.append(tempdist)
 df['Distance'] = distance
-
+print(f'distance: {distance}')
+print(f'--------------------')
 print(df.head())
 
 df['Distance2'] = [gdata.query_postal_code(24515, zipcode) for zipcode in df['ZIP']]
 print(df.head())
+
+# to create a new column pulling only the lat, long coordinates into a tuple
+df['new_column'] = list(zip(df.latitude, df.longitude))
+# or
+df['new_column'] = df[['latitude', 'longitude']].apply(tuple, axis = 1)
