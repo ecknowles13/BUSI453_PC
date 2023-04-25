fe44a7c7bb4c744e63c89e41eac4fd45e5709501
ecriggins <ecknowles13@gmail.com>
2023-04-24 20:31:35 -0400

    Fix for CA zipcodes

--- geocode_dist.py
+++ geocode_dist.py
@@ -1,5 +1,6 @@
 import pgeocode
 import pandas as pd
+import numpy as np
 df = pd.read_excel("C:/Users/ecriggins/Downloads/geocode.xlsx")
 print(df.head())
 
@@ -17,6 +18,31 @@ df['Distance2'] = [gdata.query_postal_code(24515, zipcode) for zipcode in df['ZI
 print(df.head())
 
 # to create a new column pulling only the lat, long coordinates into a tuple
-df['new_column'] = list(zip(df.latitude, df.longitude))
+#df['new_column'] = list(zip(df.latitude, df.longitude))
 # or
-df['new_column'] = df[['latitude', 'longitude']].apply(tuple, axis = 1)
+#df['new_column'] = df[['latitude', 'longitude']].apply(tuple, axis = 1)
+
+df = pd.read_csv("C:/Users/ecriggins/Downloads/data.csv")
+df = df[['AACOMAS_ID', 'ZIP', 'COUNTRY']]
+print(df.head(25).to_string())
+
+# set libraries
+ca_zip = pgeocode.Nominatim('CA')
+us_zip = pgeocode.Nominatim('US')
+
+# cast zip column to string type
+df['ZIP'] = df['ZIP'].astype(str)
+
+# create a list of all zipcodes
+zip_list = df.ZIP.values.tolist()
+
+# loop through zip codes to produce lat long coordinates
+point = []
+for z in zip_list:
+    lat = np.where(len(z) == 7, ca_zip.query_postal_code(z).latitude, us_zip.query_postal_code(z[0:5]).latitude)
+    long = np.where(len(z) == 7, ca_zip.query_postal_code(z).longitude, us_zip.query_postal_code(z[0:5]).longitude)
+    point.append((lat, long))
+
+# add column for point coordinates
+df['POINT'] = point
+print(df.head(25).to_string())
